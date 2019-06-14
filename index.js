b20cac3b94c203d0df072e7563f5a2bb2473fb61
JoeBenRob <joebenrob@gmail.com>
2019-06-14 14:54:42 +0100

    incomplete

--- index.js
+++ index.js
@@ -21,8 +21,33 @@ const makeRequest = (method, url, body) => {
 
 function read() {
     let name = document.getElementById("name").value;
+   
     makeRequest("GET", `http://www.omdbapi.com/?apikey=6ba38886&t=${name}`)
-    .then(res => {console.log(res)});
+    .then(res => {
+        
+        console.log(res)
+
+        const tableRow = document.createElement("tr");
+        const tableData1 = document.createElement("td");
+        const tableData2 = document.createElement("td");
+        const tableData3 = document.createElement("td");
+
+        const table = document.createElement("table");
+
+        tableData1.innerText = "test";
+        tableData2.innerText = "test";
+        tableData3.innerText = "test";
+
+        tableRow.appendChild(tableData1);
+        tableRow.appendChild(tableData2);
+        tableRow.appendChild(tableData3);
+
+        
+        table.appendChild(tableRow);
+
+    });
+
+
 
 }
 
